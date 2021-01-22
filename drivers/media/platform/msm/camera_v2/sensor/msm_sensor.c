ca1f0f9b2e3ef60d416335825050a43e1424f312
Peter Ardiarta <50816765+prooholic@users.noreply.github.com>
2021-01-22 15:41:52 +0700

    Update msm_sensor.c

--- drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
+++ drivers/media/platform/msm/camera_v2/sensor/msm_sensor.c
@@ -253,17 +253,7 @@ int msm_sensor_match_vendor_id(struct msm_sensor_ctrl_t *s_ctrl)
 	uint16_t vcmid = 0;
 	uint16_t lensid = 0;
 	int have_vcmid = 0;
-<<<<<<< HEAD
-#ifdef CONFIG_MACH_XIAOMI_NEW_CAMERA
-    uint16_t lensid = 0;
-    int have_lensid = 0;
-#endif
-#elif defined(CONFIG_MACH_MI)
-	uint16_t moduleflag = 0;
-#endif
-=======
 	int have_lensid = 0;
->>>>>>> 680127eedf2e (drivers: msm: camera_v2: fix new version)
 	enum cci_i2c_master_t temp_master = MASTER_0;
 
 	if (!s_ctrl) {
@@ -446,20 +436,6 @@ int msm_sensor_match_vendor_id(struct msm_sensor_ctrl_t *s_ctrl)
 		s_ctrl->sensordata->vcm_id_info->data_type);
 		have_vcmid = 1;
 	}
-<<<<<<< HEAD
-#ifdef CONFIG_MACH_XIAOMI_NEW_CAMERA
-    if (s_ctrl->sensordata->lens_id_info->lens_id_addr != 0)
-    {
-        msm_camera_cci_i2c_read(
-                                sensor_i2c_client,
-                                s_ctrl->sensordata->lens_id_info->lens_id_addr,
-                                &lensid,
-                                s_ctrl->sensordata->lens_id_info->data_type);
-        have_lensid = 1;
-    }
-#endif
-#endif
-=======
 
     if (s_ctrl->sensordata->lens_id_info->lens_id_addr != 0)
 	{
@@ -470,7 +446,6 @@ int msm_sensor_match_vendor_id(struct msm_sensor_ctrl_t *s_ctrl)
 		s_ctrl->sensordata->lens_id_info->data_type);
 		have_lensid = 1;
 	}
->>>>>>> 680127eedf2e (drivers: msm: camera_v2: fix new version)
 
 	sensor_i2c_client->cci_client->sid = temp_sid;
 	sensor_i2c_client->cci_client->cci_i2c_master = temp_master;
@@ -497,26 +472,6 @@ int msm_sensor_match_vendor_id(struct msm_sensor_ctrl_t *s_ctrl)
 				__func__, vcmid, s_ctrl->sensordata->vcm_id_info->vcm_id);
 			}
 		}
-<<<<<<< HEAD
-#ifdef CONFIG_MACH_XIAOMI_NEW_CAMERA
-        if(have_lensid == 1)
-        {
-            if (s_ctrl->sensordata->lens_id_info->lens_id != lensid)
-            {
-                pr_err("%s:match lensid if failed read lens id: 0x%x expected id 0x%x:\n",
-                       __func__, lensid, s_ctrl->sensordata->lens_id_info->lens_id);
-                rc = -1;
-                return rc;
-            }
-            else
-            {
-                pr_err("%s: read lensid id: 0x%x expected id 0x%x:\n",
-                       __func__, lensid, s_ctrl->sensordata->lens_id_info->lens_id);
-            }
-        }
-#endif
-#endif
-=======
         if(have_lensid == 1)
 		{
 			if (s_ctrl->sensordata->lens_id_info->lens_id != lensid)
@@ -532,7 +487,6 @@ int msm_sensor_match_vendor_id(struct msm_sensor_ctrl_t *s_ctrl)
 				__func__, lensid, s_ctrl->sensordata->lens_id_info->lens_id);
 			}
 		}
->>>>>>> 680127eedf2e (drivers: msm: camera_v2: fix new version)
 	}
 	pr_err("%s: read vendor id: 0x%x expected id 0x%x:\n",
 			__func__, vendorid, s_ctrl->sensordata->vendor_id_info->vendor_id);
