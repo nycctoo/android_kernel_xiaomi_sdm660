921e6af1df41f65a6839ab8327a2fdfe5e07ebb6
Peter Ardiarta <50816765+prooholic@users.noreply.github.com>
2021-01-22 16:27:49 +0700

    Update msmb_camera.h

--- include/uapi/media/msmb_camera.h
+++ include/uapi/media/msmb_camera.h
@@ -53,15 +53,7 @@
 #define MSM_CAMERA_SUBDEV_EXT          19
 #define MSM_CAMERA_SUBDEV_TOF          20
 #define MSM_CAMERA_SUBDEV_LASER_LED    21
-<<<<<<< HEAD
-#ifdef CONFIG_MACH_XIAOMI_NEW_CAMERA
 #define MSM_MAX_CAMERA_SENSORS  7
-#else
-#define MSM_MAX_CAMERA_SENSORS  6
-#endif
-=======
-#define MSM_MAX_CAMERA_SENSORS  7
->>>>>>> 680127eedf2e (drivers: msm: camera_v2: fix new version)
 
 /* The below macro is defined to put an upper limit on maximum
  * number of buffer requested per stream. In case of extremely
